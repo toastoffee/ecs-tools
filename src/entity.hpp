f725e09a119614d94525315e5570d44576ad619b
toastoffee <931391442@qq.com>
2024-07-06 19:42:45 +0800

    file create

---
+++ src/entity.hpp
@@ -0,0 +1,22 @@
+/**
+  ******************************************************************************
+  * @file           : entity.hpp
+  * @author         : toastoffee
+  * @brief          : None
+  * @attention      : None
+  * @date           : 2024/7/6
+  ******************************************************************************
+  */
+
+
+
+#ifndef ECS_TOOLS_ENTITY_HPP
+#define ECS_TOOLS_ENTITY_HPP
+
+
+class entity {
+
+};
+
+
+#endif //ECS_TOOLS_ENTITY_HPP
---
+++ src/system.hpp
@@ -0,0 +1,22 @@
+/**
+  ******************************************************************************
+  * @file           : system.hpp
+  * @author         : toastoffee
+  * @brief          : None
+  * @attention      : None
+  * @date           : 2024/7/6
+  ******************************************************************************
+  */
+
+
+
+#ifndef ECS_TOOLS_SYSTEM_HPP
+#define ECS_TOOLS_SYSTEM_HPP
+
+
+class system {
+
+};
+
+
+#endif //ECS_TOOLS_SYSTEM_HPP

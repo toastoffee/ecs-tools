824c50146f53bbead73934f64f56b91fbfb8d6c6
toastoffee <931391442@qq.com>
2024-07-06 22:18:43 +0800

    add entity manager

---
+++ src/entity_manager.hpp
@@ -0,0 +1,31 @@
+/**
+  ******************************************************************************
+  * @file           : entity_manager.hpp
+  * @author         : toastoffee
+  * @brief          : None
+  * @attention      : None
+  * @date           : 2024/7/6
+  ******************************************************************************
+  */
+
+
+
+#ifndef ECS_TOOLS_ENTITY_MANAGER_HPP
+#define ECS_TOOLS_ENTITY_MANAGER_HPP
+
+#include "types.hpp"
+
+class EntityManager {
+public:
+    EntityManager(){
+
+    }
+
+    Entity CreateEntity() {
+        
+    }
+
+};
+
+
+#endif //ECS_TOOLS_ENTITY_MANAGER_HPP

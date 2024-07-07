7fff729c1161747624b91f46d5e34dffa4be1d78
toastoffee <931391442@qq.com>
2024-07-07 19:19:06 +0800

    complete coordinator

--- src/coordinator.hpp
+++ src/coordinator.hpp
@@ -28,7 +28,66 @@ private:
 public:
 
     Coordinator() {
-        _componentManager =
+        _componentManager = std::unique_ptr<ComponentManager>();
+        _entityManager = std::unique_ptr<EntityManager>();
+        _systemManager = std::unique_ptr<SystemManager>();
+    }
+
+    Entity CreateEntity() {
+        return _entityManager->CreateEntity();
+    }
+
+    void DestroyEntity(Entity entity) {
+        _entityManager->DestroyEntity(entity);
+        _componentManager->EntityDestroyed(entity);
+        _systemManager->EntityDestroyed(entity);
+    }
+
+    template<typename T>
+    void RegisterComponent() {
+        _componentManager->RegisterComponent<T>();
+    }
+
+    template<typename T>
+    void AddComponent(Entity entity, T component) {
+        _componentManager->AddComponent<T>(entity, component);
+
+        auto signature = _entityManager->GetSignature(entity);
+        signature.set(_componentManager->GetComponentType<T>(), true);
+        _entityManager->SetSignature(entity, signature);
+
+        _systemManager->EntitySignatureChanged(entity, signature);
+    }
+
+    template<typename T>
+    void RemoveComponent(Entity entity) {
+        _componentManager->RemoveComponent<T>(entity);
+
+        auto signature = _entityManager->GetSignature(entity);
+        signature.set(_componentManager->GetComponentType<T>(), false);
+        _entityManager->SetSignature(entity, signature);
+
+        _systemManager->EntitySignatureChanged(entity, signature);
+    }
+
+    template<typename T>
+    T &GetComponent(Entity entity) {
+        return _componentManager->GetComponent<T>(entity);
+    }
+
+    template<typename T>
+    ComponentType GetComponentType() {
+        return _componentManager->GetComponentType<T>();
+    }
+
+    template<typename T>
+    std::shared_ptr<T> RegisterSystem() {
+        return _systemManager->RegisterSystem<T>();
+    }
+
+    template<typename T>
+    void SetSystemSignature(Signature signature) {
+        _systemManager->SetSignature<T>(signature);
     }
 };
 

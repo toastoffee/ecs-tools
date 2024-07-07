40a0b60bda76226970b5aea2fe36790988dec28b
toastoffee <931391442@qq.com>
2024-07-07 13:56:53 +0800

    complete component manager

--- src/component_manager.hpp
+++ src/component_manager.hpp
@@ -30,13 +30,78 @@ private:
 
 private:
     template<typename T>
-    std::shared_ptr<ComponentArray<T>> GetComponentArray(){
+    std::shared_ptr<ComponentArray<T>> GetComponentArray() {
         const char* typeName = typeid(T).name();
 
         assert(_componentTypes.find(typeName) != _componentTypes.end()
                 && "component not registered before use.");
 
         return std::static_pointer_cast<ComponentArray<T>>(_componentArrays[typeName]);
+    }
+
+public:
+    template<typename T>
+    void RegisterComponent() {
+        const char* typeName = typeid(T).name();
+
+        assert(_componentTypes.find(typeName) == _componentTypes.end()
+               && "Registering component type more than once.");
+
+        // add this component type to the map
+        _componentTypes.insert({typeName, _nextComponentType});
+
+        // create a componentArray pointer and add it to the map
+        _componentArrays.insert({typeName, std::make_shared<ComponentArray<T>>()});
+
+        _nextComponentType++;
+    }
+
+    template<typename T>
+    ComponentType GetComponentType() {
+        const char* typeName = typeid(T).name();
+
+        assert(_componentTypes.find(typeName) != _componentTypes.end()
+               && "Component not registered before use.");
+
+        return _componentTypes[typeName];
+    }
+
+    template<typename T>
+    void AddComponent(Entity entity, T component) {
+        const char* typeName = typeid(T).name();
+
+        assert(_componentTypes.find(typeName) != _componentTypes.end()
+               && "Component not registered before use.");
+
+        GetComponentArray<T>()->InsertData(entity, component);
+    }
+
+    template<typename T>
+    void RemoveComponent(Entity entity) {
+        const char* typeName = typeid(T).name();
+
+        assert(_componentTypes.find(typeName) != _componentTypes.end()
+               && "Component not registered before use.");
+
+        GetComponentType<T>()->RemoveData(entity);
+    }
+
+    template<typename T>
+    T& GetComponent(Entity entity) {
+        const char* typeName = typeid(T).name();
+
+        assert(_componentTypes.find(typeName) != _componentTypes.end()
+               && "Component not registered before use.");
+
+        return GetComponentType<T>()->GetData(entity);
+    }
+
+    void EntityDestroyed(Entity entity) {
+        // notify each component array that an entity has been destroyed
+        for(auto const & pair : _componentArrays){
+            auto const& componentArray = pair.second;
+            componentArray->EntityDestroyed(entity);
+        }
     }
 };
 

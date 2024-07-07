041af9dcc2f78eff0dd83e46003e85acb60c3b96
toastoffee <931391442@qq.com>
2024-07-07 11:42:04 +0800

    update entity manager

--- src/entity_manager.hpp
+++ src/entity_manager.hpp
@@ -15,14 +15,48 @@
 
 #include "types.hpp"
 
+#include <queue>
+#include <algorithm>
+
+
 class EntityManager {
+private:
+    // unused entity IDs
+    std::queue<Entity> _availableEntities{};
+
+    // signatures correspond to entity ID
+    std::array<Signature, MAX_ENTITIES> _signatures{};
+
+    // all living entities count;
+    Entity _livingEntitiesCount{};
+
 public:
     EntityManager(){
-
+        // initialize the queue with all entity IDs
+        for (Entity entity = 0; entity < MAX_ENTITIES; ++entity) {
+            _availableEntities.push(entity);
+        }
     }
 
     Entity CreateEntity() {
-        
+        assert(_livingEntitiesCount < MAX_ENTITIES && "living entities count larger than extent");
+
+        // take an unused ID from the queue;
+        Entity id = _availableEntities.front();
+        _availableEntities.pop();
+        _livingEntitiesCount++;
+
+        return id;
+    }
+
+    void DestroyEntity(Entity entity) {
+        assert(entity < MAX_ENTITIES && "entity id out of range");
+
+        // BUG: remains a bug that when you try to destroy an entity that already in _availableEntities
+
+        _signatures[entity].reset();
+        _availableEntities.push(entity);
+        _livingEntitiesCount--;
     }
 
 };
